9024efb0d190cfd1264e1ba9ef0c3c96253b74a8
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 06:18:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,38 +1,39 @@
-def create_an_empty_array []
+def create_an_empty_array
+  []
 end
 
 def create_an_array
-  
+  audi [s4, s5, tt, ttrs]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  audi.push (r8)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  audi.unshift (a4)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  audi.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  audi.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  audi [0]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  audi [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  audi [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  audi [a4] = "rs5"
 end
