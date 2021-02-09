f8bbc3ebf9f361cb2e8389ae166c30110d4b31f4
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 06:53:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["I", "am", "really", "learning", "arrays!"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[0]
 end
 
